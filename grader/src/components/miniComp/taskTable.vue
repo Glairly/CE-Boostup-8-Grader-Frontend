3e2a912b33336771a86465c1ea838c6881ef27ed
Phumphathai Chansriwong <ouoam2555@gmail.com>
2020-07-12 16:08:48 +0700

    prevent null to lowercase
    when click X to clear text in search text box it will null

--- grader/src/components/miniComp/taskTable.vue
+++ grader/src/components/miniComp/taskTable.vue
@@ -702,7 +702,7 @@ export default {
           return el[key]
             .toString()
             .toLocaleLowerCase()
-            .includes(search.toLocaleLowerCase());
+            .includes((search || "").toLocaleLowerCase());
         });
       });
     },
